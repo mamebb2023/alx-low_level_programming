cf3c116bcef655a43779f99b02f5d3b3d051f121
Mohammednur <mamebb2023@gmail.com>
2022-12-11 00:24:19 +0300

    Print the alphabate without q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print the alphabet exept q and e
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z' && i != 'e' && i != 'q'; i++)
+		putchar(i);
+	
+	putchar('\n');
+
+	return (0);
+}
