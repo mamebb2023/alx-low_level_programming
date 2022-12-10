8d959a5c59961ece56df91060e8118e8991b111a
Mohammednur <mamebb2023@gmail.com>
2022-12-11 00:46:35 +0300

    Print numbers form 0 - 9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints from 0 - 9
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		putchar('i');
+
+	putchar('\n');
+
+	return (0);
+}
