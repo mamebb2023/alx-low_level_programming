88d578baedba7f65d1f2846942b16e8af364cd5d
Mohammednur <mamebb2023@gmail.com>
2022-12-13 11:37:27 +0300

    Prints the alphabet 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,18 @@
 #include "main.h"
 
 /**
- * main - Prints the alphabet 10x
+ * print_alphabet_x10 - Prints the alphabet 10x
  */
 
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
 
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 97; j < 123; j++)
+			_putchar(j);
+		_putchar('\n');
+	}
+}
