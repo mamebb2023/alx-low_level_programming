7b5f91cbcaa495e3d37acf14880881e76f52a502
Mohammednur <mamebb2023@gmail.com>
2022-12-14 11:57:51 +0300

    Prints the minit in jack bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Print full minute of jack bauer
+ */
+
+void jack_bauer(void)
+{
+	int x;
+	int j;
+
+	for (x = 0; x <= 23; x++)
+	{
+		for (j = 0; j <= 59; j++)
+		{
+			_putchar((x / 10) + '0');
+			_putchar((x % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
