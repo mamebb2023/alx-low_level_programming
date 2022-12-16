0b11c9b31f1c995214ab5fd7c78cebd31b819c3c
Mohammednur <mamebb2023@gmail.com>
2022-12-16 23:11:04 +0300

    Multiplies two intigers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two intigers
+ * @a: first intiger
+ * #b: second intiger
+ *
+ * Return: the results
+ */
+
+int mul(int a, int b)
+{
+	int i;
+
+	i = a * b;
+
+	return (i);
+}
