2885a45400af83fb073251e77d3bd306841672d1
Mohammednur <mamebb2023@gmail.com>
2022-12-21 11:29:12 +0300

    Concatinates n bytes of a string to another

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *_strncat - Concatinates n bytes of a string to another
+ * @dest: the destination
+ * @src: the source
+ * @n: the n bytes
+ *
+ * Return: the resunting dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	unsigned int i;
+	int j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+
+	for (j = 0; j < n && src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+
+	return (dest);
+
+}
