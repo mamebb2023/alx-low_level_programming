fa8f87dcd4943fcbfd284c7590af795d96855b53
Mohammednur <mamebb2023@gmail.com>
2023-01-04 12:09:40 +0300

    Puts a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Puts a stirng
+ * @s: pointer to a char
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
