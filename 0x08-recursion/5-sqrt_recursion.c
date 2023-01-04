9cb12aef6dfd4ff775a59086c7ceee4b3480a9bd
Mohammednur <mamebb2023@gmail.com>
2023-01-04 16:33:26 +0300

    Calculates the squareroot of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * the_sqrt - finds the number
+ * @a: the base number
+ * @b: the square root number
+ *
+ * Return: the result
+ */
+
+int the_sqrt(int a, int b)
+{
+	if (b * b == a)
+		return (b);
+	else if (b * b == a)
+		return (-1);
+
+	return (the_sqrt(a, b + 1));
+}
+
+/**
+ * _sqrt_recursion - Calculates the natural square root
+ * @n: the number
+ *
+ * Return: the result
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (the_sqrt(n, 1));
+}
