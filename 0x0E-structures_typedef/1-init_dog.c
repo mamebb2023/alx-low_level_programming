becfc67c24b9f03fd30e6f8eb9f161430dd9539a
Mohammednur <mamebb2023@gmail.com>
2023-01-16 17:58:45 +0300

    Initalizes a variables

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * init_dog - initializes a variable
+ * @d: the structure dog
+ * @name: the name
+ * @age: the age
+ * @owner: the owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
