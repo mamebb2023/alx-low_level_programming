2e94d4e4f06ceb7dac5aa99658798d9fa537784c
Mohammednur <mamebb2023@gmail.com>
2023-01-16 17:52:50 +0300

    A structer diclaration

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,15 +1,32 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * struct dog - a structure for a dog
+ * struct dog - a structure for a profile of a dog
  * @name: the name
  * @age: the age
- * @owner: the owner of the dog
+ * @owner: the owner
  */
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+
+#endif
