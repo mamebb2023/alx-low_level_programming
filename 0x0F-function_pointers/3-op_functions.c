cf46c4a79ab2dac2f76767a6a352d112534ea6da
Mohammednur <mamebb2023@gmail.com>
2023-01-18 21:14:19 +0300

    Performes simple operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Add two integers
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the result number
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Substructs two integers
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the result number
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Multiplies two integers
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the result number
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Diviedes two integers
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the result number
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod -Finds the reminder of two integers
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the result number
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
