c5db87ad6f31d0bd86a3ce3633705a9f63f57617
Mohammednur <mamebb2023@gmail.com>
2023-01-18 11:20:00 +0300

    Concatinates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -33,7 +33,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; s1[i]; i++)
 		p[i] = s1[i];
 
-	for (j = 0; j < n && s2[j]; j++, i++)
+	for (j = 0; s2[j] && j < n; j++)
 		p[i] = s2[j];
 
 	p[i] = '\0';
