4d08287b384bc00cb3b76e1946e88afd3457445f
Mohammednur <mamebb2023@gmail.com>
2023-01-18 12:08:23 +0300

    Allocates n bytes of memory

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,7 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	p = malloc(nmem * size);
+	p = malloc(nmemb * size);
 
 	if (p == NULL)
 		return (NULL);
