16c89517cfbcc02d1af4697d8957ee4bef23857e
Mohammednur <mamebb2023@gmail.com>
2023-01-26 14:36:43 +0300

    Lists the number of elements in list_t

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * list_len - Lists the number of elements in list_t
+ * @h: the pointer to the struct list_s
+ *
+ * Return: the length of the elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	const list_t *temp;
+	size_t i;
+
+	temp = h;
+	i = 0;
+
+	while(temp != NULL)
+	{
+		temp = temp->next;
+		i++;
+	}
+
+	return (i);
+}
