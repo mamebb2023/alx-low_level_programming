9260f5f512e25d2ff4c82349b4487515c931db5a
Mohammednur <mamebb2023@gmail.com>
2023-01-31 12:35:35 +0300

    Frees a list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,13 +7,16 @@
 
 void free_listint2(listint_t **head)
 {
+	listint_t *temp;
+
 	if (head == NULL)
 		return;
 
 	while (*head)
 	{
+		temp = (*head)->next;
 		free(*head);
-		*head = *head->next;
+		*head = temp;
 	}
 	head = NULL;
 }
