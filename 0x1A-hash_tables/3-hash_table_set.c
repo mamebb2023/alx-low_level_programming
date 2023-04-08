1e19e1c7420a5e5d2ae7f1b7040e3e126cc58a51
Mohammednur <mamebb2023@gmail.com>
2023-04-08 12:12:19 +0300

    Set the element in the table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,42 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Set an element in the table
+ * @ht: the table
+ * @key: the key of the element
+ * @value: the value of the element
+ *
+ * Return: 1 if success else 0
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	int index;
+	hash_node_t *item, *current_item;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value  == NULL)
+		return (0);
+
+	item = malloc(sizeof(hash_node_t));
+	if (item == NULL)
+		return (0);
+
+	item->key = malloc(strlen(key) + 1);
+	item->value = malloc(strlen(value) + 1);
+	if (item->key == NULL || item->value == NULL)
+		return (0);
+
+	strcpy(item->key, key);
+	strcpy(item->value, value);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	current_item = ht->array[index];
+
+	if (current_item == NULL)
+		ht->array[index] = item;
+	else
+	{
+		if (strcmp(current_item->key, key) == 0)
+			strcpy(ht->array[index]->value, value);
+	}
+	return (1);
+}
