78ba83c17f2fdcc9e3cd89b29863835a4fedfa8a
Mohammednur <mamebb2023@gmail.com>
2023-04-08 10:48:47 +0300

    Create the # table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,11 +16,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	table->size = size;
-	table->array = (hash_node_t **) malloc(sizeof(hash_node_t) * size);
+	table->array = malloc(sizeof(hash_node_t *) * size);
 	if (table->array == NULL)
 		return (NULL);
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < table->size; i++)
 		table->array[i] = NULL;
 
 	return (table);
