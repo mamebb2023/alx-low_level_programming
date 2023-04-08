62339f5ff44b337a4525bff496cc9c91af44aae2
Mohammednur <mamebb2023@gmail.com>
2023-04-08 14:30:58 +0300

    Get the element

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Retrives a value
+ * @ht: the table
+ * @key: the key to find
+ *
+ * Return: the value of the key retrived
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t* item;
+
+	index = key_index((const unsigned char *)key, ht->size);
+	item = ht->array[index];
+
+	if (item != NULL)
+	{
+		if (strcmp(item->key, key) == 0)
+			return item->value;
+	}
+	return (NULL);	
+}
